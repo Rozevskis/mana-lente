dbf6bea5e1fd5b2b882dd801e298c0cf2cfe8e21
Kristofers Rozevskis <kristofers.rozevskis@gmail.com>
2025-04-24 21:45:16 +0300

    API return all articles

--- backend/src/articles/articles.controller.ts
+++ backend/src/articles/articles.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ArticlesService } from './articles.service';
+import { Article } from './article.entity';
 
 @Controller('articles')
-export class ArticlesController {}
+export class ArticlesController {
+  constructor(private readonly articlesService: ArticlesService) {}
+
+  @Get()
+  findAll(): Promise<Article[]> {
+    return this.articlesService.findAll();
+  }
+}
